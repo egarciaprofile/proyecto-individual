b8dea56595f7a41640630b3303e06b2543b3294f
Esteban García Pérez <egarcia@profile.es>
2024-04-30 14:42:27 +0200

    Seat and Venue controllers

--- src/main/java/dev/estebangperez/ticket/controller/TicketerSeatApi.java
+++ src/main/java/dev/estebangperez/ticket/controller/TicketerSeatApi.java
@@ -1,2 +1,16 @@
-package dev.estebangperez.ticket.controller;public interface TicketerSeatApi {
+package dev.estebangperez.ticket.controller;
+
+import dev.estebangperez.ticket.model.TicketerSeatDTO;
+import dev.estebangperez.ticket.utils.TicketerApi;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Set;
+
+@RequestMapping(TicketerApi.BASE_PATH + "seat")
+public interface TicketerSeatApi {
+    @GetMapping("/event/{eventId}")
+    ResponseEntity<Set<TicketerSeatDTO>> findSeatsByEventId(@PathVariable Long eventId);
 }
--- src/main/java/dev/estebangperez/ticket/controller/impl/TicketerSeatController.java
+++ src/main/java/dev/estebangperez/ticket/controller/impl/TicketerSeatController.java
@@ -1,2 +1,21 @@
-package dev.estebangperez.ticket.controller.impl;public class TicketerSeatController {
+package dev.estebangperez.ticket.controller.impl;
+
+import dev.estebangperez.ticket.controller.TicketerSeatApi;
+import dev.estebangperez.ticket.model.TicketerSeatDTO;
+import dev.estebangperez.ticket.service.impl.TicketerSeatServiceImpl;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
+
+@RestController
+@AllArgsConstructor
+public class TicketerSeatController implements TicketerSeatApi {
+    private final TicketerSeatServiceImpl ticketerSeatService;
+
+    @Override
+    public ResponseEntity<Set<TicketerSeatDTO>> findSeatsByEventId(Long eventId) {
+        return ResponseEntity.ok(ticketerSeatService.findSeatsByEventId(eventId));
+    }
 }
